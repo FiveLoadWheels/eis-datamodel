877ee96276397f2f43e13191c3657d2162afc94f
Nick Ng <nick.doth@gmail.com>
2017-04-07 14:33:14 +0800

    pluralized table names
    "A table, which is a dataset of `thing`, contains many `things`"

--- eis.sql
+++ eis.sql
@@ -1,16 +1,5 @@
-DROP TABLE IF EXISTS 'Customer';
-DROP TABLE IF EXISTS 'Order';
-DROP TABLE IF EXISTS 'Address';
-DROP TABLE IF EXISTS 'CreditCard';
-DROP TABLE IF EXISTS 'Product';
-DROP TABLE IF EXISTS 'Pro_consist_Acc';
-DROP TABLE IF EXISTS 'End_consist_Acc';
-DROP TABLE IF EXISTS 'Cart';
-DROP TABLE IF EXISTS 'EndProduct';
-DROP TABLE IF EXISTS 'Supplier';
-DROP TABLE IF EXISTS 'Accessory';
 
-CREATE TABLE 'Customer' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'customers' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'password' VARCHAR NOT NULL,
 						 'first_name' VARCHAR NOT NULL,
 						 'last_name' VARCHAR NOT NULL,
@@ -21,44 +10,44 @@ CREATE TABLE 'Customer' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'tel' CHAR);
 						 
 
-CREATE TABLE 'Order' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'orders' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 		      			'create_time' DATETIME NOT NULL,
 					'modify_time' DATETIME NOT NULL,
 					'status' INTEGER DEFAULT '1' NOT NULL,
 					'cid' INTEGER NOT NULL);
 					  
-CREATE TABLE 'Address' ('address' VARCHAR NOT NULL,
+CREATE TABLE 'addresses' ('address' VARCHAR NOT NULL,
 						'cid' INTEGER NOT NULL);
 						
-CREATE TABLE 'CreditCard' ('card_num' CHAR PRIMARY KEY NOT NULL,
+CREATE TABLE 'credit_cards' ('card_num' CHAR PRIMARY KEY NOT NULL,
 							'validate_code' CHAR NOT NULL,
 							'cid' INTEGER NOT NULL);
 							
-CREATE TABLE 'Product' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'products' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						'model_name' VARCHAR NOT NULL,
 						'primary_price' INTEGER NOT NULL,
 						'screen_size' INTEGER NOT NULL);
 						
-CREATE TABLE 'Pro_consist_Acc' ('pid' INTEGER NOT NULL,
+CREATE TABLE 'pro_consist_acc' ('pid' INTEGER NOT NULL,
 								'aid' INTEGER NOT NULL);
 								
-CREATE TABLE 'End_consist_Acc' ('eid'  NOT NULL,
+CREATE TABLE 'end_consist_acc' ('eid'  NOT NULL,
 								'aid'  NOT NULL);
 								
-CREATE TABLE 'Cart' ('cart_id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'cart_items' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 					 'cid' INTEGER NOT NULL,
 					 'eid' INTEGER NOT NULL);
 					 
-CREATE TABLE 'EndProduct' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'end_products' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 							'status' INTEGER DEFAULT '1' NOT NULL,
 							'oid' INTEGER NOT NULL,
 							'serial_number' VARCHAR UNIQUE NOT NULL);
 							
-CREATE TABLE 'Supplier' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'suppliers' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'email' VARCHAR UNIQUE NOT NULL,
 						 'tele' CHAR NOT NULL, 'address' VARCHAR NOT NULL);
 						 
-CREATE TABLE 'Accessory' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+CREATE TABLE 'accessories' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						  'model_name' VARCHAR NOT NULL,
 						  'purchase_price' DOUBLE NOT NULL,
 						  'quantity' INTEGER NOT NULL,
