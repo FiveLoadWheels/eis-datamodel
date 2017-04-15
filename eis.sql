6fffab54382b16129fc8ccff8b1881d2cdc403b5
Nick Ng <nick.doth@gmail.com>
2017-04-15 16:01:41 +0800

    Update eis.sql

--- eis.sql
+++ eis.sql
@@ -1,22 +1,22 @@
 
 CREATE TABLE 'customers' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'password' VARCHAR NOT NULL,
-						 'first_name' VARCHAR NOT NULL,
-						 'last_name' VARCHAR NOT NULL,
+						 'firstName' VARCHAR NOT NULL,
+						 'lastName' VARCHAR NOT NULL,
 						 'ctime' DATETIME NOT NULL,
 						 'mtime' DATETIME NOT NULL,
-						 'last_login' DATETIME NOT NULL,
+						 'lastLogin' DATETIME NOT NULL,
 						 'email' VARCHAR UNIQUE NOT NULL,
 						 'tel' CHAR);
 						 
 
 CREATE TABLE 'personnel' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'password' VARCHAR NOT NULL,
-						 'first_name' VARCHAR NOT NULL,
-						 'last_name' VARCHAR NOT NULL,
+						 'firstName' VARCHAR NOT NULL,
+						 'lastName' VARCHAR NOT NULL,
 						 'ctime' DATETIME NOT NULL,
 						 'mtime' DATETIME NOT NULL,
-						 'last_login' DATETIME NOT NULL,
+						 'lastLogin' DATETIME NOT NULL,
 						 'email' VARCHAR UNIQUE NOT NULL,
 						 'tel' CHAR,
 			 			 'role' CHAR NOT NULL);
@@ -31,12 +31,12 @@ CREATE TABLE 'addresses' ('address' VARCHAR NOT NULL,
 						'cid' INTEGER NOT NULL);
 						
 CREATE TABLE 'credit_cards' ('card_num' CHAR PRIMARY KEY NOT NULL,
-							'validate_code' CHAR NOT NULL,
+							'validateCode' CHAR NOT NULL,
 							'cid' INTEGER NOT NULL);
 							
 CREATE TABLE 'product_models' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-						'model_name' VARCHAR NOT NULL,
-						'primary_price' INTEGER NOT NULL,
+						'modelName' VARCHAR NOT NULL,
+						'primaryPrice' INTEGER NOT NULL,
 						'screen_size' INTEGER NOT NULL);
 						
 CREATE TABLE 'pro_consist_acc' ('pid' INTEGER NOT NULL,
@@ -51,16 +51,17 @@ CREATE TABLE 'cart_items' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 					 
 CREATE TABLE 'products' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 							'status' INTEGER DEFAULT '1' NOT NULL,
+			 				'model' INTEGER NOT NULL,
 							'oid' INTEGER NOT NULL,
-							'serial_number' VARCHAR UNIQUE NOT NULL);
+							'serialNumber' VARCHAR UNIQUE NOT NULL);
 							
 CREATE TABLE 'suppliers' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 						 'email' VARCHAR UNIQUE NOT NULL,
 						 'tele' CHAR NOT NULL, 'address' VARCHAR NOT NULL);
 						 
 CREATE TABLE 'accessories' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-						  'model_name' VARCHAR NOT NULL,
-						  'purchase_price' DOUBLE NOT NULL,
+						  'modelName' VARCHAR NOT NULL,
+						  'purchasePrice' DOUBLE NOT NULL,
 						  'quantity' INTEGER NOT NULL,
 						  'type' VARCHAR NOT NULL,
-						  'supplier_id' INTEGER NOT NULL);
+						  'supplierId' INTEGER NOT NULL);
